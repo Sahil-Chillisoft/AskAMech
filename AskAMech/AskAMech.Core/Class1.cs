2bab7cd14dd7e1f4f263f1b028225e9c3c504ead
Sahil Muniram <sahil.muniram@chillisoft.co.za>
2020-08-27 23:50:47 +0200

    Infrastructure and Core project creation and setup.

---
+++ AskAMech/AskAMech.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AskAMech.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ AskAMech/AskAMech.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AskAMech.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
