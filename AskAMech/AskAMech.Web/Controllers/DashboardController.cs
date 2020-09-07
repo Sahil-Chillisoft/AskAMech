8388730541d45b0f3961aa87784ecdf71fd7bbd6
Sahil Muniram <sahil.muniram@chillisoft.co.za>
2020-09-07 23:19:32 +0200

    Creation of User Dashboard.

---
+++ AskAMech/AskAMech.Web/Controllers/DashboardController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AskAMech.Web.Controllers
+{
+    public class DashboardController : Controller
+    {
+        [HttpGet]
+        public IActionResult UserDashboard()
+        {
+            return View();
+        }
+    }
+}
---
+++ AskAMech/AskAMech.Web/wwwroot/js/Dashboard/userDashboard.js
@@ -0,0 +1,19 @@
+﻿function Navigate(id) {
+    switch (id) {
+        case 'divMyQuestions':
+            window.location.href = '';
+            break;
+        case 'divMyAnswers':
+            window.location.href = '';
+            break;
+        case 'divLatestQuestions':
+            window.location.href = '';
+            break;
+        case 'divNewQuestions':
+            window.location.href = '';
+            break;
+        case 'divMyProfile':
+            window.location.href = '';
+            break;
+    }
+}
