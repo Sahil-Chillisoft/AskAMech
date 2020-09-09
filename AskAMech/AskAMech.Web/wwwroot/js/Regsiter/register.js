b436247925ade9388e0fa94bc2fa749ac4df8573
andile <andiletseku@gmail.com>
2020-09-09 21:43:02 +0200

    merge reg.js

--- AskAMech/AskAMech.Web/wwwroot/js/Regsiter/register.js
+++ AskAMech/AskAMech.Web/wwwroot/js/Regsiter/register.js
@@ -13,46 +13,38 @@
                 registerModal.find('.modal').modal('show');
             }
         });
-<<<<<<< HEAD:AskAMech/AskAMech.Web/wwwroot/js/Regsiter/register.js
-=======
 
-        $("#ConfirmPassword").keyup(function () {
-            if ($("#Password").val() != $("#ConfirmPassword").val()) {
-                $("#msg").html("Password do not match").css("color", "red");
+        registerModal.on('click',
+            '[data-save="modal"]',
+            function (event) {
+                event.preventDefault();
+
+                var $form = $(this).parents('.modal').find('form');
+                $.validator.unobtrusive.parse($form);
+
+                if ($form.valid()) {
+                    var formData = $form.serialize();
+                    $.ajax({
+                        url: 'Home/Register',
+                        type: 'POST',
+                        cache: false,
+                        data: formData
+                    })
+                        .done(function (data) {
+                            var modalContent = $('.modal-body', data);
+                            registerModal.find('.modal-body')
+                                .replaceWith(modalContent);
+                            var isValid = modalContent.find('[name="IsValid"]')
+                                .val() === 'True';
+                            if (isValid) {
+                                registerModal.find('.modal').modal('hide');
+                                //TODO: Load the dashboard page if valid 
+                            }
+                        });
+                }
+                else {
+                    return false;
+                }
             });
->>>>>>> fixing reg:AskAMech/AskAMech.Web/wwwroot/js/Register.js
     });
-
-    registerModal.on('click',
-        '[data-save="modal"]',
-        function (event) {
-            event.preventDefault();
-
-            var $form = $(this).parents('.modal').find('form');
-            $.validator.unobtrusive.parse($form);
-
-            if ($form.valid()) {
-                var formData = $form.serialize();
-                $.ajax({
-                    url: 'Home/Register',
-                    type: 'POST',
-                    cache: false,
-                    data: formData
-                })
-                    .done(function (data) {
-                        var modalContent = $('.modal-body', data);
-                        registerModal.find('.modal-body')
-                            .replaceWith(modalContent);
-                        var isValid = modalContent.find('[name="IsValid"]')
-                            .val() === 'True';
-                        if (isValid) {
-                            registerModal.find('.modal').modal('hide');
-                            //TODO: Load the dashboard page if valid 
-                        }
-                    });
-            }
-            else {
-                return false;
-            }
-        });
 });
