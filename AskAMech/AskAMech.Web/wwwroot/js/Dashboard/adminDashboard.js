cbe208542c59cd0c5a69f81ed326b1f15a3861a8
andile <andiletseku@gmail.com>
2020-09-15 19:38:09 +0200

    admin dashboard icons to be changed

--- AskAMech/AskAMech.Web/wwwroot/js/Dashboard/adminDashboard.js
+++ AskAMech/AskAMech.Web/wwwroot/js/Dashboard/adminDashboard.js
@@ -1,1 +1,19 @@
-﻿
+﻿function Navigate(id) {
+    switch (id) {
+        case 'divEmployees':
+            window.location.href = '';
+            break;
+        case 'divNewEmployees':
+            window.location.href = '';
+            break;
+        case 'divUserFromEmployee':
+            window.location.href = '';
+            break;
+        case 'divNewQuestionCategory':
+            window.location.href = '';
+            break;
+        case 'divUserRole':
+            window.location.href = '';
+            break;
+    }
+}
