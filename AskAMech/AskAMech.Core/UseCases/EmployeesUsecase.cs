d977302c1f55c56c42ef4c5f6071e19cf0b8e7c7
andile <andiletseku@gmail.com>
2020-10-09 17:32:20 +0200

    changes

---
+++ AskAMech/AskAMech.Core/UseCases/EmployeesUsecase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AskAMech.Core.UseCases.Requests;
+using AskAMech.Core.UseCases.Responses;
+using AskAMech.Core.Domain;
+using AskAMech.Core.UseCases.Interfaces;
+using AskAMech.Core.Gateways.Repositories;
+
+namespace AskAMech.Core.UseCases
+{
+   public class EmployeesUsecase : IEmployeesUsecase
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public EmployeesUsecase(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository ?? throw new ArgumentNullException(nameof(employeeRepository));
+
+        }
+
+        public void Execute(EmployeeRequests requests, IPresenter presenter)
+        {
+
+        }
+    }
+}
