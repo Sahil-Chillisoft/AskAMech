e8bca73dd8eba4d5886dd32953cfebeae6a756c8
Sahil Muniram <sahil.muniram@chillisoft.co.za>
2020-10-17 21:57:48 +0200

    Implement search panel toggle on question list page

---
+++ AskAMech/AskAMech.Web/wwwroot/js/Question/index.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    var search = $('#Search').val();
+    var category = $('#CategoryId').val();
+
+    if (search !== '' || category !== '') {
+        var searchDiv = $('#searchDiv');
+        searchDiv.show();
+    }
+
+    $('#SearchButton').click(function (event) {
+        event.preventDefault();
+        var searchDiv = $('#searchDiv');
+        searchDiv.toggle();
+    });
+});
