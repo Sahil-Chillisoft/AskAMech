abc0edf87084b007f37856592b92b14ca571d66d
andile <andiletseku@gmail.com>
2020-10-19 21:41:40 +0200

    adding views for create role page

---
+++ AskAMech/AskAMech.Web/wwwroot/js/Roles/CreateRoles.js
@@ -0,0 +1,41 @@
+﻿$(document).ready(function () {
+
+    $('#create').click(function (event) {
+        event.preventDefault();
+
+        var $form = $('#RoleCreateForm');
+        $.validator.unobtrusive.parse($form);
+
+        if ($form.valid()) {
+            var formData = $form.serialize();
+            $.ajax({
+                url: '/Roles/Index',
+                type: 'POST',
+                cache: false,
+                enctype: 'multipart/form-data',
+                processData: false,
+                data: formData,
+                success: function (data) {
+                    if (data.success) {
+                        displaySuccessModal();
+                    } else {
+                        $('#ErrorMessage').text(data.message);
+                    }
+                }
+            });
+        }
+    });
+
+    function displaySuccessModal() {
+        var successModal = $('#createSuccessDiv');
+        $.ajax({
+            url: '/Roles/CreateSuccess',
+            type: 'GET',
+            cache: false,
+            success: function (data) {
+                successModal.html(data);
+                successModal.find('.modal').modal('show');
+            }
+        });
+    }
+});
---
+++ AskAMech/AskAMech.Web/wwwroot/js/Roles/createSuccess.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+
+    $('#AddNewRole').click(function (event) {
+        event.preventDefault();
+        window.location.href = '/Roles/Index';
+    });
+
+    $('#Dashboard').click(function (event) {
+        event.preventDefault();
+        window.location.href = '/Dashboard/AdminDashboard';
+    });
+
+});
