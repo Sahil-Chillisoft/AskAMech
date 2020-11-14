df4a68eb6de4d73942242059a902a1e56131bc05
andile <andiletseku@gmail.com>
2020-11-14 12:14:59 +0200

    merge

--- AskAMech/AskAMech.Web/wwwroot/js/Regsiter/register.js
+++ AskAMech/AskAMech.Web/wwwroot/js/Regsiter/register.js
@@ -9,6 +9,7 @@
             type: 'GET',
             cache: false,
             success: function (data) {
+<<<<<<< HEAD:AskAMech/AskAMech.Web/wwwroot/js/Regsiter/register.js
                 registerModal.html(data);
                 registerModal.find('.modal').modal('show');
             }
@@ -48,3 +49,47 @@
             });
     });
 });
+=======
+                verifyModal.html(data);
+            verifyModal.find('.modal').modal('show');
+        });
+    }
+
+});
+
+
+//        updateModal.on('click',
+//    '[data-save="modal"]',
+//    function (event) {
+//        event.preventDefault();
+
+//        var $form = $(this).parents('.modal').find('form');
+//        $.validator.unobtrusive.parse($form);
+
+//        if ($form.valid()) {
+//            var formData = $form.serialize();
+//            updateModal.find('[id="loadingImg"]').removeClass('hide');
+
+//            $.ajax({
+//                url: 'User/UpdatePassword',
+//                type: 'POST',
+//                cache: false,
+//                data: formData
+//            }).done(function (data) {
+//                updateModal.find('[id="loadingImg"]').addClass('hide');
+
+//                var modalContent = $('.modal-body', data);
+//                updateModal.find('.modal-body').replaceWith(modalContent);
+
+//                var isValid = modalContent.find('[name="IsValid"]').val() === 'True';
+//                if (isValid) {
+//                    updateModal.find('.modal').modal('hide');
+//                    $('.modal-backdrop').remove();
+//                    window.location.href = '/Dashboard/UserDashboard';
+//                }
+//            });
+//        }
+//    });
+//    });
+//});
+>>>>>>> WIP: edit EditPasswordModal:AskAMech/AskAMech.Web/wwwroot/js/User/EditPasswordModal.js
