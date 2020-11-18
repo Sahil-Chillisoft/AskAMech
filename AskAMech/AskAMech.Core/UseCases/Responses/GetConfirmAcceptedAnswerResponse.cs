be92dfa47d5d65423db5f9a01aabb02c1772f2fa
Sahil Muniram <sahil.muniram@chillisoft.co.za>
2020-11-18 23:31:13 +0200

    WIP: confirm answer as accepted answer on ViewQuestion page

---
+++ AskAMech/AskAMech.Core/UseCases/Responses/GetConfirmAcceptedAnswerResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace AskAMech.Core.UseCases.Responses
+{
+    public class GetConfirmAcceptedAnswerResponse
+    {
+        public string Title { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ AskAMech/AskAMech.Web/wwwroot/js/Answers/answer.js
@@ -0,0 +1,45 @@
+﻿$(document).ready(function () {
+
+    var questionId = $('#QuestionId').val();
+    var isAcceptedAnswer = $('#AcceptedAnswer');
+
+    $('#AcceptedAnswer').change(function (event) {
+        event.preventDefault();
+        if (isAcceptedAnswer.prop("checked") === true)
+            updateAcceptedAnswerStatus(questionId, true);
+        else
+            updateAcceptedAnswerStatus(questionId, false);
+    });
+});
+
+function updateAcceptedAnswerStatus(questionId, isAcceptedAnswer) {
+    $.ajax({
+        url: '/Answer/UpdateAcceptedAnswer',
+        type: 'POST',
+        cache: false,
+        data: {
+            'id': questionId,
+            'isAcceptedAnswer': isAcceptedAnswer
+        },
+        success: function (data) {
+            if (data.success) {
+                console.log('Update Success');
+                //Display Modal 
+            }
+        }
+    });
+}
+
+function displayConfirmAcceptedAnswerModal() {
+    $.ajax({
+        url: '/Answer/ConfirmAcceptedAnswer',
+        type: 'GET',
+        cache: false,
+        success: function (data) {
+            if (data.success) {
+                console.log('Update Success');
+                //Display Modal 
+            }
+        }
+    });
+}
